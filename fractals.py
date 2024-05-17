c2f798e3b4b8f1ac59a2aafeb923d9734302e45a
arcane <12jspeake@gmail.com>
2024-05-17 21:32:00 +0100

    Add custom cmap and render region for fractals

--- fractals.py
+++ fractals.py
@@ -20,40 +20,41 @@ def burning_ship(step, z, c):
         return burning_ship(step+1, z_next, c)
     return step, z_next, c
 
-def render(fractal_function):
-    real_range = np.linspace(-2.5, 1.5, SAMPLES, dtype=complex)
-    imag_range = np.linspace(-1.5, 1.5, SAMPLES, dtype=complex)
+def render(fractal_function, region, cmap):
+    real_range = np.linspace(region[0][0], region[0][1], SAMPLES, dtype=complex)
+    imag_range = np.linspace(region[1][0], region[1][1], SAMPLES, dtype=complex)
     image_range = imag_range * 1j
     real, imag = np.meshgrid(real_range, imag_range)
     c_grid = real + (imag * 1j)
     z_grid = np.zeros_like(c_grid)
     step_grid = np.zeros_like(c_grid, dtype=int)
     step_grid, z_grid, c_grid = fractal_function(step_grid, z_grid, c_grid)
-    plt.contourf(real, imag, step_grid, cmap="inferno_r")
+    plt.contourf(real, imag, step_grid, cmap=cmap)
+    ax = plt.gca()
+    ax.set_ylim(ax.get_ylim()[::-1])
     plt.show()
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--fractaltype",
+        "--fractal",
         type=str,
         nargs=1,
         required=True,
-        help="--fractaltype [mandelbrot, burning_ship]",
+        help="Usage: --fractal [mandelbrot, burning_ship]",
     )
     arguments = parser.parse_args()
-    fractal_type = arguments.fractaltype[0]
+    fractal_type = arguments.fractal[0]
     if fractal_type == "mandelbrot":
-        return mandelbrot
+        return mandelbrot, [[-2.5, 1.5],[-1.5, 1.5]], "viridis_r"
     if fractal_type == "burning_ship":
-        print(burning_ship)
-        return burning_ship
+        return burning_ship, [[-2.5, 1.5],[-2, 1]], "inferno_r"
     else:
         raise Exception
 
 def main():
-    recursion_function = parse_arguments()
+    recursion_function, region, cmap = parse_arguments()
     recursion_function_vec = np.vectorize(recursion_function)
-    render(recursion_function_vec)
+    render(recursion_function_vec, region, cmap)
 
 main()
